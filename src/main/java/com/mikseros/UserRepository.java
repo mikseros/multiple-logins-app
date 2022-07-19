912526550c61381b3af29bfce8625a468204b147
mikseros <mmadynski@gmail.com>
2022-07-19 22:44:08 +0200

    User Repository

---
+++ src/main/java/com/mikseros/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mikseros;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+	public List<User> findByEmail(String email);
+}
