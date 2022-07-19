b275c06fecd269242363ddf10c025a279060ef80
mikseros <mmadynski@gmail.com>
2022-07-19 22:24:02 +0200

    Initial commit

---
+++ src/main/java/com/mikseros/MultipleLoginsAppApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleLoginsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleLoginsAppApplication.class, args);
+	}
+
+}
