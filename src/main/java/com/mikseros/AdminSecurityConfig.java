4a2ec01583f00e3a3e3c7ef96ecb4f85be551b94
mikseros <mmadynski@gmail.com>
2022-07-22 22:28:00 +0200

    Admin Security Configuration

--- src/main/java/com/mikseros/AdminSecurityConfig.java
+++ src/main/java/com/mikseros/AdminSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +14,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AdminSecurityConfig {
 	
 	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Bean
 	public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/").permitAll();
+		
+		http.antMatcher("/admin/**")
+			.authorizeRequests().anyRequest().hasAuthority("ADMIN")
+			.and().formLogin()
+				.loginPage("/admin/login")
+				.usernameParameter("email")
+				.loginProcessingUrl("/admin/login")
+				.defaultSuccessUrl("/admin/home")
+				.permitAll()
+			.and()
+				.logout().logoutUrl("/admin/logout")
+				.logoutSuccessUrl("/");
+		
 		return http.build();
 	}
 }
