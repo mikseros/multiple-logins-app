a1b6346122087cc28eb4e8497f91e5135b03d572
mikseros <mmadynski@gmail.com>
2022-07-22 22:29:08 +0200

    Function findByEmail() updated

--- src/main/java/com/mikseros/UserRepository.java
+++ src/main/java/com/mikseros/UserRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
-	public List<User> findByEmail(String email);
+//	public List<User> findByEmail(String email);
+	public User findByEmail(String email);
 }
