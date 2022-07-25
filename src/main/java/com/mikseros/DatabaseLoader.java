6a343c70def7b7aec2f2d3f68a9b79bf2223e98c
mikseros <mmadynski@gmail.com>
2022-07-25 14:16:16 +0200

    Database Loader updated -> new constructor etc.

--- src/main/java/com/mikseros/DatabaseLoader.java
+++ src/main/java/com/mikseros/DatabaseLoader.java
@@ -8,24 +8,33 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mikseros.admin.User;
 import com.mikseros.admin.UserRepository;
+import com.mikseros.customer.Customer;
+import com.mikseros.customer.CustomerRepository;
 
 @Configuration
 public class DatabaseLoader {
 
-	private UserRepository repo;
+	private UserRepository userRepo;
+	private CustomerRepository customerRepo;
 
-	public DatabaseLoader(UserRepository repo) {
+	public DatabaseLoader(UserRepository userRepo, CustomerRepository customerRepo) {
 		super();
-		this.repo = repo;
+		this.userRepo = userRepo;
+		this.customerRepo = customerRepo;
 	}
-	
+
 	@Bean
 	public CommandLineRunner initializeDatabase() {
 		return args -> {
-			User user1 = new User("admin@xxx.com", "123456");
+			User user1 = new User("user@xxx.com", "123456");
 			User user2 = new User("mark@xxx.com", "123456");
 			
-			repo.saveAll(List.of(user1, user2));
+			userRepo.saveAll(List.of(user1, user2));
+			
+			Customer customer1 = new Customer("client1@xxx.com", "123456", "John Doe");
+			Customer customer2 = new Customer("client2@xxx.com", "123456", "Jane Doe");
+			
+			customerRepo.saveAll(List.of(customer1, customer2));
 			
 			System.out.println("Sample database initialized");
 		};
